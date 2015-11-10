c6edb3189e55f2a7bbc0b3e2363445709796f6fd
Balsa Bojic <balsha.bojic@gmail.com>
2015-11-10 16:03:11 +0100

    Strategy blank interface deleted

--- src/main/java/smartgrid/simulation/strategy/Strategy.java
+++
@@ -1,7 +0,0 @@
-package smartgrid.simulation.strategy;
-
-public interface Strategy {
-	public double getSolarProduction();
-	public double getWindProduction();
-	public double getBioProduciton();
-}
