f429f6e8127a178ffda122868379330733109dc5
Balsa Bojic <balsha.bojic@gmail.com>
2015-11-12 14:33:22 +0100

    EV, Street sensors added, smg info added, smg cut from and add to network, smg production

--- src/main/java/smartgrid/simulation/strategy/StrategyManager.java
+++ src/main/java/smartgrid/simulation/strategy/StrategyManager.java
@@ -1,5 +1,10 @@
 package smartgrid.simulation.strategy;
 
+import java.util.HashMap;
+import java.util.Map.Entry;
+
+import smartgrid.simulation.village.answers.SmgAnswer;
+
 public class StrategyManager {
 
 	private int solarSensor;
@@ -12,25 +17,56 @@ public class StrategyManager {
 	
 	private double consumption;
 	
-	public StrategyManager(int solarSensor, double solarProduction, double windProduction, double bioProduction, double consumption) {
+	private int evSensor;
+	private int streetSensor;
+	
+	private HashMap<String, SmgAnswer> smgData;
+	
+	public StrategyManager(int solarSensor, double solarProduction, 
+			double windProduction, double bioProduction, 
+			double consumption, HashMap<String, SmgAnswer> smgData) {
 		this.solarSensor = solarSensor; 
 		this.solarProduction = solarProduction;
 		this.windProduction = windProduction;
 		this.bioProduction = bioProduction;
 		this.consumption = consumption;
+		this.smgData = smgData;
 		this.windSensor = 100;
 		this.bioSensor = 100;
+		
+		// Sensor for electric vehicles, if we have overproduction we are charging vehicles, if not we need to turn off
+		// default value is 1,  and if we have underproduction we need to turn it off 
+		this.evSensor = 1;
+		// Sensor for street lights that can be controlled
+		// if 1 then we have enough energy, if 0 then we need to turn off them - reduce consumption also!
+		this.streetSensor = 1;
 	}
 	
 	public void optimizeProduction() {
 		solarProduction = solarProduction * ((double)solarSensor / 100);
-		double production = windProduction + bioProduction + solarProduction;
+		
+		// Ask SMG node to connect again in the network
+		double smgProduction = 0;
+		if (smgData != null) {
+			for (Entry<String, SmgAnswer> smg : smgData.entrySet()) {
+				smgProduction += smg.getValue().getProduction();
+			}
+		}
+		
+		double production = windProduction + bioProduction + solarProduction + smgProduction;
 		
 		// TODO we have overproduction - need to reduce the production (we are considering always 95% of production,
 		// because we want to be sure that it will always be +5% energy in network 
 		// priority is bio < wind < sun, so we are first turning off bio
 		if (production > consumption) {
 			double difference = production - consumption;
+			
+			// We have overproduction we can call smg to connect to grid normally
+			if (smgData != null) {
+				for (Entry<String, SmgAnswer> smg : smgData.entrySet()) {
+					smg.getValue().sendData("/api/openhab/dummy.wrapper.dummy_switchgridconnected/1");
+				}
+			}
 			
 			// if overproduction is lower then bioProduction then reduce the production of bioProduction
 			if (difference < bioProduction) {
@@ -65,8 +101,39 @@ public class StrategyManager {
 			}
 		}
 		else {
+			// Set maximum production
 			windSensor = 100;
 			bioSensor = 100;
+			
+			// When we have underproduction turn off the EV first
+			evSensor = 0;
+			
+			double difference = consumption - production;
+			
+			// Ask SMG node to disconnect from a network
+			if (smgData != null) {
+				for (Entry<String, SmgAnswer> smg: smgData.entrySet()) {
+					double smgConsumption = smg.getValue().getConsumption();
+					if (difference > smgConsumption) {
+						smg.getValue().sendData("/api/openhab/dummy.wrapper.dummy_switchgridconnected/0");
+						difference -= smgConsumption;
+						smg.getValue().setConsumption(0.0);
+						smg.getValue().setProduction(0.0);
+						consumption -= smgConsumption;
+					}
+					else if (difference < smgConsumption && difference > 0){
+						smg.getValue().sendData("/api/openhab/dummy.wrapper.dummy_switchgridconnected/0");
+						difference = 0;
+						smg.getValue().setConsumption(0.0);
+						smg.getValue().setProduction(0.0);
+						consumption -= smgConsumption;
+					}
+				}
+			}
+			
+			if (difference > 0) {
+				streetSensor = 0;
+			}
 		}
 	}
 
@@ -124,6 +191,30 @@ public class StrategyManager {
 
 	public void setConsumption(double consumption) {
 		this.consumption = consumption;
+	}
+
+	public int getEvSensor() {
+		return evSensor;
+	}
+
+	public void setEvSensor(int evSensor) {
+		this.evSensor = evSensor;
+	}
+
+	public int getStreetSensor() {
+		return streetSensor;
+	}
+
+	public void setStreetSensor(int streetSensor) {
+		this.streetSensor = streetSensor;
+	}
+
+	public HashMap<String, SmgAnswer> getSmgData() {
+		return smgData;
+	}
+
+	public void setSmgData(HashMap<String, SmgAnswer> smgData) {
+		this.smgData = smgData;
 	}
 	
 }
