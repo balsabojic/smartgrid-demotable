ff225ac635a79c027ec893f1090a2f620665d14c
Balsa Bojic <balsha.bojic@gmail.com>
2015-11-12 14:25:22 +0100

    Send methods and baseurl moved to the answer, profiles changed with live data

--- src/main/java/smartgrid/simulation/village/SmgModel.java
+++ src/main/java/smartgrid/simulation/village/SmgModel.java
@@ -1,44 +1,29 @@
 package smartgrid.simulation.village;
 
 import java.time.LocalDateTime;
-import java.util.Date;
 import java.util.HashSet;
 import java.util.LinkedList;
-import java.util.Random;
-
-import javax.crypto.Mac;
-import javax.crypto.spec.SecretKeySpec;
-
-import org.apache.commons.codec.binary.Base64;
-
-import com.sun.jersey.api.client.ClientResponse;
-import com.sun.jersey.api.client.WebResource;
-import com.sun.jersey.api.client.Client;
-
 import akka.advancedMessages.ErrorAnswerContent;
 import akka.basicActors.ActorOptions;
 import akka.basicActors.LoggingMode;
 import akka.basicMessages.AnswerContent;
 import akka.basicMessages.RequestContent;
+import akka.systemActors.GlobalTime;
+import helper.lastProfil.ConsumptionProfleFortiss;
+import helper.lastProfil.ProductionProfleFortiss;
 import resultSaving.NoSave;
 import smartgrid.simulation.village.answers.SmgAnswer;
 import topology.ActorTopology;
 
 public class SmgModel extends BasicVillageModel {
 	
-	// Default admin user data taken from the database
-	String accessKey = "68a45057-5734-4dad-9f86-ab9e32c4506e";
-	String secretKey = "jg9e65dui5272c45uds3qrf3b8gc71crjq4raq43";
-	
 	private String name;
 	private LocalDateTime time;
 	private SmgAnswer answer;
-	private String smgUrl;
 	
 	public SmgModel(String name, String smgUrl) {
 		this.name = name;
-		this.smgUrl = smgUrl;
-		this.answer = new SmgAnswer(name);
+		this.answer = new SmgAnswer(name, smgUrl);
 	}
 
 	@Override
@@ -61,78 +46,27 @@ public class SmgModel extends BasicVillageModel {
 	@Override
 	public void makeDecision() {
 		// Test URL
-		//String requestURL = smgUrl + "/api/health/online";
+		//String requestURL = "/api/health/online";
 		
-		String requestConsumption = smgUrl + "/api/openhab/items/solarlog.wrapper.solar_feed-in_watt/state";
-		String result = sendRequest(requestConsumption);
-		answer.setConsumption(result);
+		String requestConsumption = "/api/openhab/items/dummy.wrapper.dummy_consumption/type/ConsumptionPowermeter";
+		String result = answer.sendRequest(requestConsumption);
+		answer.setConsumption(Double.valueOf(result) / 1000);
 		
-		String requestBattery = smgUrl + "/api/openhab/items/sunny.wrapper.battery_percentage/state";
-		result = sendRequest(requestBattery);
-		answer.setBatteryCapacity(result);
 		
-		String requestProduction = smgUrl + "/api/openhab/items/solarlog.wrapper.solar_feed-in_watt/state";
-		result = sendRequest(requestProduction);
-		answer.setProduction(result);
-	}
-	
-	private String sendRequest(String requestURL) {
-		if (requestURL.contains("?")) {
-			requestURL = requestURL + "&accesskey=" + accessKey;
-		} else {
-			requestURL = requestURL + "?accesskey=" + accessKey;
-		}
-		requestURL = requestURL + "&timestamp=" + new Date().getTime() / 1000;
-		requestURL = requestURL + "&authversion=1";
-		requestURL = requestURL + "&nonce=" + new Random().nextLong();
-
-		String signature = getSignature(secretKey, requestURL);
-
-		requestURL = requestURL + "&signature=" + signature;
-
-		try {
-
-			Client client = Client.create();
-
-			WebResource webResource = client.resource(requestURL);
-
-			ClientResponse response = webResource.accept("application/json").get(ClientResponse.class);
-
-			if (response.getStatus() != 200) {
-				throw new RuntimeException("Failed : HTTP error code : " + response.getStatus());
-			}
-
-			String output = response.getEntity(String.class);
-
-			return output;
-
-		} catch (Exception e) {
-
-			e.printStackTrace();
-
-		}
-		return "";
+		String requestProduction = "/api/openhab/items/dummy.wrapper.dummy_generation/type/ProductionPowermeter";
+		result = answer.sendRequest(requestProduction);
+		answer.setProduction(Double.valueOf(result) / 1000);
 		
-	}
-	
-	private static String getSignature(String secretKey, String requestURL) {
-		String signature = "";
-		String hash = "";
+		String requestBattery = "/api/openhab/items/dummy.wrapper.dummy_battery/type/Battery";
+		result = answer.sendRequest(requestBattery);
+		answer.setBatteryCapacity(Double.valueOf(result));
 		
-		try {
-			Mac sha256_HMAC = Mac.getInstance("HmacSHA1");
-			SecretKeySpec secret_key = new SecretKeySpec(secretKey.getBytes(), "HmacSHA1");
-			sha256_HMAC.init(secret_key);
-			hash = Base64.encodeBase64String(sha256_HMAC.doFinal(requestURL.getBytes()));
-			
-			signature = java.net.URLEncoder.encode(hash, "utf8");
-			
-			return signature;
-		} catch (Exception e) {
-			System.out.println(e);
-		}
-		
-		return "";
+		// SMG Profiles when not using live data
+//		LocalDateTime time = GlobalTime.currentTime;
+//		Double production = ProductionProfleFortiss.getLoadFortissProduction(time);
+//		answer.setProduction(production / 1000);		
+//		Double consumption = ConsumptionProfleFortiss.getLoadFortissConsumption(time);
+//		answer.setConsumption(consumption / 1000);
 	}
 
 	@Override
